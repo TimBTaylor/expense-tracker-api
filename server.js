fdd8426962a096a78b028a8425c30768745dc711
TimBTaylor <79194271+TimBTaylor@users.noreply.github.com>
2021-07-16 18:07:47 -0400

    updated cors

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
@@ -13,19 +14,28 @@ const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Database"));
 
+let allowedOrigins = [
+  "https:/localhost:3000",
+  "https://expensetrackerbytim.herokuapp.com/",
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        // If a specific origin isn’t found on the list of allowed origins
+        let message =
+          "The CORS policy for this application doesn’t allow access from origin " +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
+
 app.use(express.json());
-
-app.use((res, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "*");
-
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-    return set.status(200).json({});
-  }
-
-  next();
-});
 
 const userRouter = require("./routes/user");
 
