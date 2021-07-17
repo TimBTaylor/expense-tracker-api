3fbd920142bdbef1ffd390db35ebca248fcabc84
TimBTaylor <79194271+TimBTaylor@users.noreply.github.com>
2021-07-16 21:06:33 -0400

    updates

--- server.js
+++ server.js
@@ -16,24 +16,10 @@ const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Database"));
 
-let allowedOrigins = [
-  "http://localhost:3000",
-  "https://expensetrackerbytim.herokuapp.com",
-];
-
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        // If a specific origin isn’t found on the list of allowed origins
-        let message =
-          "The CORS policy for this application doesn’t allow access from origin " +
-          origin;
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    },
+    origin: "*",
+    methods: ["GET, POST, DELETE"],
   })
 );
 
